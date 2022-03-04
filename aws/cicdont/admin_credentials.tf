15d013db70de6f706a827cfba4c7fe770e2403af
Nick Frichette <nick@frichetten.com>
2022-03-03 19:33:51 -0600

    Removed admin permissions from admin creds Decided there is no reason to have admin credentials for the player's AWS accounts stored on a random EC2 instance. Instead, we can just call them admin creds without any permissions

--- aws/cicdont/admin_credentials.tf
+++ aws/cicdont/admin_credentials.tf
@@ -2,26 +2,6 @@ resource "aws_iam_user" "terraform_admin_user" {
   name = "terraform_admin_user"
 }
 
-resource "aws_iam_user_policy" "terraform_admin_policy" {
-  name = "terraform_admin_policy"
-  user = aws_iam_user.terraform_admin_user.name
-
-  policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": [
-        "*"
-      ],
-      "Effect": "Allow",
-      "Resource": "*"
-    }
-  ]
-}
-EOF
-}
-
 resource "aws_iam_access_key" "terraform_admin_user_access_key" {
   user = aws_iam_user.terraform_admin_user.name
 }
