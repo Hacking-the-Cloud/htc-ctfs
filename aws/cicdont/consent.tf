d7d4217dd9684508eb5250f4d3ba56052e59ad3f
Nick Frichette <nick@frichetten.com>
2022-03-06 21:01:58 -0600

    Finalized README

--- aws/cicdont/consent.tf
+++ aws/cicdont/consent.tf
@@ -1,6 +1,6 @@
 variable "consent" {
   type        = string
-  description = "Do you understand that you are responsible for the costs associated with deploying this infrastructure using Terraform. While attempts have been made to ensure that costs are as low as possible, this CTF was created by memebers of Hacking the Cloud's community and no guarantees can be made. Please do not modify this configuration and please ensure you use `terraform destroy` to clean it up. Please do not deploy this in an account with irrecoverable data. Do you consent? (yes/no)"
+  description = "By playing this CTF you understand that you are responsible for the costs associated with deploying this infrastructure using Terraform. While attempts have been made to ensure that costs are as low as possible, this CTF was created by memebers of Hacking the Cloud's community and no guarantees can be made. Please do not modify this configuration and please ensure you use `terraform destroy` to clean it up. Please do not deploy this in an account with irrecoverable data. Do you consent? (yes/no)"
 
   validation {
     condition     = lower(var.consent) == "yes"
